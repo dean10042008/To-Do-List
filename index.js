f9d3f43e10e4807389cfcd1ca2630ac3bcdfa2dd
dean10042008 <dean.yurukov2008@gmail.com>
2024-08-30 14:02:38 +0300

    major update. add option to edit the tasks

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const plusButton = document.getElementById('addButton');
 const itemContainer = document.querySelector('.items-container');
 let initial = document.getElementById('initial-item');
 
-let i = 0;
+let i = 1;
 
 function addTask() {
-
-    i++;
 
     let text = inputField.value;
 
@@ -22,6 +20,7 @@ function addTask() {
                         
             <div class="button-wrapper">
                 <button id="done-${i}" class="doneButton" type="button"><i class="fa-solid fa-check"></i></button>
+                <button id="edit-${i}" class="editButton" type="button"><i class="fa-solid fa-pen"></i></button>
                 <button id="delete-${i}" class="deleteButton" type="button"><i class="fa-solid fa-trash"></i></button>
             </div>
         `;
@@ -41,6 +40,10 @@ function addTask() {
 
         document.getElementById(`done-${i}`).addEventListener("click", () => setTaskAsDone(div));
         document.getElementById(`delete-${i}`).addEventListener("click", () => deleteTask(div));
+        document.getElementById(`edit-${i}`).addEventListener("click", () => editTask(div));
+
+
+        i++;
     }
 }
 
@@ -69,6 +72,72 @@ function setTaskAsDone(div) {
     button.style.color = 'gray';
 }
 
+function editTask(div) {
+    const buttons = div.querySelector('.button-wrapper');
+    let id = buttons.children[0].id.split("-")[1];
+    
+    let previousText = div.querySelector('p').textContent.trim();
+
+    let input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('value', previousText);
+    input.className = 'input';
+    input.id = `input-${id}`;
+
+    let button = document.createElement('button');
+    button.className = 'finishEditing';
+    button.id = `edit-${id}`;
+
+    let icon = document.createElement('i');
+    icon.className = 'ri-check-double-line';
+
+    button.appendChild(icon);
+
+    div.innerHTML = `
+        ${input.outerHTML}
+
+        ${button.outerHTML}
+    `;
+
+    document.getElementById(`edit-${id}`).addEventListener('click', () => {
+        finishEdit(div, document.getElementById(`input-${id}`).value, buttons, previousText, id);
+    });
+}
+
+function finishEdit(div, newText, buttons, prevText, i) {
+    let localStorageArr = JSON.parse(localStorage.getItem('item'));
+    const indexText = localStorageArr.indexOf(prevText);
+    
+    if (indexText !== -1) {
+        localStorageArr.splice(indexText, 1, newText);
+
+
+        div.innerHTML = `
+            <p>${newText}</p>
+
+            ${buttons.outerHTML}
+        `;
+    }
+    else {
+        const indexDoneText = localStorageArr.indexOf(`${newText}-done`);
+        localStorageArr.splice(indexDoneText, 1, `${newText}-done`);
+
+        div.innerHTML = `
+            <p>
+                <s>${newText}</s>
+            </p>
+
+            ${buttons.outerHTML}
+        `;
+    }
+    
+    localStorage.setItem('item', JSON.stringify(localStorageArr));
+
+    document.getElementById(`done-${i}`).addEventListener("click", () => setTaskAsDone(div));
+    document.getElementById(`delete-${i}`).addEventListener("click", () => deleteTask(div));
+    document.getElementById(`edit-${i}`).addEventListener("click", () => editTask(div));
+}
+
 function deleteTask(div) {
     let text = div.querySelector('p').textContent;
 
@@ -95,12 +164,9 @@ function deleteTask(div) {
 
 document.addEventListener('DOMContentLoaded', () => {
     let arr = JSON.parse(localStorage.getItem('item'));
-    let i = 0;
 
     arr.forEach((item) => {
         initial.style.display = 'none';
-
-        i++;
 
         if (item.includes('-done')) {
             let text = item.split('-done')[0];
@@ -113,6 +179,7 @@ document.addEventListener('DOMContentLoaded', () => {
                             
                 <div class="button-wrapper">
                     <button id="done-${i}" class="doneButton" type="button" disabled style="border: 1px solid gray; scale: 1; color: gray"><i class="fa-solid fa-check"></i></button>
+                    <button id="edit-${i}" class="editButton" type="button"><i class="fa-solid fa-pen"></i></button>
                     <button id="delete-${i}" class="deleteButton" type="button"><i class="fa-solid fa-trash"></i></button>
                 </div>
             `;
@@ -121,6 +188,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             document.getElementById(`done-${i}`).addEventListener("click", () => setTaskAsDone(div));
             document.getElementById(`delete-${i}`).addEventListener("click", () => deleteTask(div));
+            document.getElementById(`edit-${i}`).addEventListener("click", () => editTask(div));
         }
         else {
             let div = document.createElement('div');
@@ -131,6 +199,7 @@ document.addEventListener('DOMContentLoaded', () => {
                             
                 <div class="button-wrapper">
                     <button id="done-${i}" class="doneButton" type="button"><i class="fa-solid fa-check"></i></button>
+                    <button id="edit-${i}" class="editButton" type="button"><i class="fa-solid fa-pen"></i></button>
                     <button id="delete-${i}" class="deleteButton" type="button"><i class="fa-solid fa-trash"></i></button>
                 </div>
             `;
@@ -139,6 +208,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
             document.getElementById(`done-${i}`).addEventListener("click", () => setTaskAsDone(div));
             document.getElementById(`delete-${i}`).addEventListener("click", () => deleteTask(div));
+            document.getElementById(`edit-${i}`).addEventListener("click", () => editTask(div));
         }
+
+        i++;
     })
 })
